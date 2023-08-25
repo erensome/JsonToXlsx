f316263d8583f9a6d1170986652d02e4e0572f44
erensome <erensome1@gmail.com>
2023-08-25 16:25:26 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Aspose.Cells;
+using Aspose.Cells.Utility;
+
+namespace JsonToXlsx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Workbook workbook = new Workbook();
+            Worksheet sheet = workbook.Worksheets[0];
+
+            string workingDirectory = Environment.CurrentDirectory;
+            //Path
+            string filePath = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+
+            filePath = Path.Combine(filePath, "./Info/Data.json");
+
+            // Read JSON file
+            string jsonInput = File.ReadAllText(filePath);
+
+            // Set json layout options
+            JsonLayoutOptions options = new JsonLayoutOptions();
+            options.ArrayAsTable = true;
+
+            JsonUtility.ImportData(jsonInput, sheet.Cells, 0, 0, options);
+
+            workbook.Save("importedData.xlsx");
+
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
